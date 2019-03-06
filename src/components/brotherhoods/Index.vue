03a09b177cc6ac0c5c6f4a5ce474990fb35da75d
Mayra Rodriguez <mers85@gmail.com>
2019-03-06 12:37:47 +0100

    added message reorder

--- src/components/brotherhoods/Index.vue
+++ src/components/brotherhoods/Index.vue
@@ -2,8 +2,11 @@
   <section class="columns is-multiline">
     <div class="column is-10 is-offset-1">
 
-      <draggable v-model="brotherhoods" class="columns is-gap is-multiline is-mobile">
-        <div class="column is-4" v-for='brotherhood in brotherhoods' v-bind:key="brotherhood.id">
+      <div class="title is-5 has-text-left">
+        <p class="has-text-primary">Puedes reordenar el orden de salto de las hermandades haciendo "drag and drop"</p>
+      </div>
+      <draggable v-model="brotherhoods" @change="handleChange" class="columns is-gap is-multiline is-mobile">
+        <div class="column is-4" v-for='(brotherhood, index) in brotherhoods' v-bind:key="brotherhood.id">
           <div class="card">
             <div class="card-content">
               <div class="media">
@@ -13,7 +16,7 @@
                   </figure>
                 </div>
                 <div class="media-content">
-                  <p class="subtitle is-6">Orden de salto# {{brotherhood.jump_order}}</p>
+                  <p class="subtitle is-6">Orden de salto# {{index + 1}}</p>
                   <p class="title is-5 has-text-left">{{brotherhood.name}}</p>
                   <p class="subtitle is-6">contacto: {{brotherhood.contact_email}}</p>
                   <p class="is-italic"> Fundada: {{brotherhood.foundation_date}}</p>
@@ -35,6 +38,7 @@
 
 <script>
 import draggable from 'vuedraggable'
+import HTTP from '../../http-common'
 
 export default {
   components: {
@@ -45,6 +49,25 @@ export default {
   },
   mounted: function () {
     this.$store.dispatch('BrotherhoodStore/index')
+  },
+  methods: {
+    handleChange () {
+      let list = this.brotherhoods.map((brotherhood, index) => {
+        return { id: brotherhood.id, order: index + 1 }
+      })
+      HTTP({
+        method: 'put',
+        url: '/sort/',
+        data: list
+      })
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+      console.log(list)
+    }
   }
 }
 </script>
